04744a535e6ae96af24947a4c0514f6bb3df9186
Kiet_Nguyen <kietnguyen711998@gmail.com>
2020-03-01 13:11:06 +0700

    Practic 2

---
+++ src/bai3/bai3.java
@@ -0,0 +1,28 @@
+package bai3;
+
+import java.util.Scanner;
+
+public class bai3 {
+
+	public static void main(String args[]){     
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter first number to find GCD :");
+        int number1 = scanner.nextInt();
+        System.out.println("Please enter second number to find GCD :");
+        int number2 = scanner.nextInt();
+      
+        System.out.println("GCD of two numbers " + number1 +" and " 
+                           + number2 +" is :" + findGCD(number1,number2));
+      
+      
+    }
+
+    private static int findGCD(int number1, int number2) {
+        //base case
+        if(number2 == 0){
+            return number1;
+        }
+        return findGCD(number2, number1%number2);
+    }
+
+}
---
+++ src/bai4/bai4.java
@@ -0,0 +1,18 @@
+package bai4;
+
+import java.util.Scanner;
+
+public class bai4 {
+
+	public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+	System.out.print("Enter number:");        
+	int n=s.nextInt();
+        for(int i=1; i <= 10; i++)
+        {
+            System.out.println(n+" * "+i+" = "+n*i);
+        }
+    }
+
+}
